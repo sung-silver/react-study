6c28fcb560ec53ec48b5fa777b7985cd910823cb
SungEun Kim <81363864+sung-silver@users.noreply.github.com>
2026-02-21 01:33:44 +0900

    study: todo 서비스 구현 (#2)
    * feat: 기능 요구사항 정의
    
    * feat: 투두앱 input 영역 및 버튼 생성
    
    * feat: 투두 리스트 아이템을 관리할 todoList 정의
    
    * feat: TodoBoard 컴포넌트 정의
    
    * feat: TodoItem 컴포넌트 구현
    
    * feat: TodoItem 스타일링
    
    * feat: props를 이용하여 값을 컴포넌트에 전달
    
    * docs: PR template 작성
    
    * feat: 할일 삭제 기능 구현

---
+++ src/component/TodoBoard.tsx
@@ -0,0 +1,21 @@
+import TodoItem from "./TodoItem";
+import type { Todo } from "../types/type";
+
+function TodoBoard({
+  todoList,
+  onDelete,
+}: {
+  todoList: Todo[];
+  onDelete: (id: string) => void;
+}) {
+  return (
+    <div>
+      <h1>Todo List</h1>
+      {todoList.map((item) => (
+        <TodoItem key={item.id} item={item} onDelete={() => onDelete(item.id)} />
+      ))}
+    </div>
+  );
+}
+
+export default TodoBoard;
---
+++ src/component/TodoItem.tsx
@@ -0,0 +1,20 @@
+import type { Todo } from "../types/type";
+
+function TodoItem({
+  item,
+  onDelete,
+}: {
+  item: Todo;
+  onDelete: () => void;
+}) {
+  return (
+    <div className="todo-item">
+      <div>{item.text}</div>
+      <button className="delete-button" onClick={onDelete}>
+        Delete
+      </button>
+    </div>
+  );
+}
+
+export default TodoItem;
