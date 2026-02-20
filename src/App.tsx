30151cdd6017362cb4c8c6af986e94a4dc4547e9
sung-silver <jinna0319@gmail.com>
2026-02-20 18:29:14 +0900

    feat: 투두앱 input 영역 및 버튼 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 
 // 1. 인풋창과 버튼이 있다
 // 2. 인풋창에 값을 입력하고 버튼을 클릭하면 아이템이 추가된다
 // 3. 아이템 삭제 버튼을 누르면 삭제가 가능하다
 function App() {
-  return <></>;
+  const [inputValue, setInputValue] = useState("");
+
+  return (
+    <main>
+      <input
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button>추가</button>
+    </main>
+  );
 }
 
 export default App;
